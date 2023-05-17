6bf9c99a8f3a87c5641bd5b5d7bff7c6f4415122
Kumar <110259496+immu2able@users.noreply.github.com>
2023-05-17 23:24:47 +0530

    check in with main

---
+++ app/src/main/java/vo/Server.java
@@ -0,0 +1,22 @@
+package vo;
+
+public class Server {
+  private String serverName;
+  private String ipAddress;
+
+    public String getServerName() {
+        return serverName;
+    }
+
+    public void setServerName(String serverName) {
+        this.serverName = serverName;
+    }
+
+    public String getIpAddress() {
+        return ipAddress;
+    }
+
+    public void setIpAddress(String ipAddress) {
+        this.ipAddress = ipAddress;
+    }
+}
